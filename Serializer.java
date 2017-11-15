de425ee480f1fe3b7d6e1e13750cce0cc7c709ef
Janet Leahy <j.leahy886@gmail.com>
2017-11-15 00:51:25 -0700

    Stores values and object references

--- Serializer.java
+++ Serializer.java
@@ -6,9 +6,10 @@ import org.jdom2.*;
 
 
 public class Serializer {
+	static LinkedList<Object> objects;
 	
 	public static org.jdom2.Document serialize(Object obj) {
-		LinkedList<Object> objects = new LinkedList<Object>();
+		objects = new LinkedList<Object>();
 		
 		Document document = new Document();
 		
@@ -50,7 +51,51 @@ public class Serializer {
 		fieldElem.setAttribute("name", field.getName());
 		fieldElem.setAttribute("declaringclass", obj.getClass().getName());
 
-		
+		Object value = null;
+		try {
+			field.setAccessible(true);
+			value = field.get(obj);
+			String content = "";
+			System.out.println(field.getType());
+
+			if (value != null && (field.getType().isPrimitive() || field.getType() == String.class)) {
+				if (value.getClass() == Integer.class) {
+					content += field.getInt(obj);
+				} else if (value.getClass() == Double.class) {
+					content += field.getDouble(obj);
+				} else if (value.getClass() == Short.class) {
+					content += field.getShort(obj);
+				} else if (value.getClass() == Long.class) {
+					content += field.getLong(obj);
+				} else if (value.getClass() == Float.class) {
+					content += field.getFloat(obj);
+				} else if (value.getClass() == Character.class) {
+					content += field.getChar(obj);
+				} else if (value.getClass() == Boolean.class) {
+					content += field.getBoolean(obj);
+				} else if (value.getClass() == Byte.class) {
+					content += field.getByte(obj);
+				} else if (value.getClass() == String.class) {
+					content += value;
+				}
+				
+				Element valueElem = new Element("value");
+				valueElem.addContent(content);
+				fieldElem.addContent(valueElem);
+
+			}
+			else if (value != null){
+				//field contains reference to an object
+				content += System.identityHashCode(value);
+				Element refElem = new Element("reference");
+				refElem.addContent(content);
+				fieldElem.addContent(refElem);
+				objects.add(value);
+			}
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
 		return fieldElem;
 	}
 }
